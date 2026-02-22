3129037edd651fcd0e5cd0665a76a69db7fac6f9
Pavel Maslov <dnfanl@gmail.com>
2026-02-22 21:55:40 +0100

    Added pet provider

--- functions/roll.go
+++ functions/roll.go
@@ -3,6 +3,8 @@ package functions
 import (
 	"errors"
 	"math/rand"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -29,4 +31,27 @@ func RollMultipleDice(amount int, dimensions int) (int, error) {
 		sum += result
 	}
 	return sum, nil
+}
+
+func RollDice(diceToRoll string) (int, error) {
+	diceInfo := strings.Split(diceToRoll, "d")
+	if len(diceInfo) != 2 {
+		return 0, errors.New("Invalid format. Use NdM (e.g. 2d6)")
+	}
+
+	number := 1
+	if diceInfo[0] != "" {
+		var err error
+		number, err = strconv.Atoi(diceInfo[0])
+		if err != nil {
+			return 0, errors.New("Invalid number. Use NdM (e.g. 2d6)")
+		}
+	}
+
+	var err error
+	dimensions, err := strconv.Atoi(diceInfo[1])
+	if err != nil {
+		return 0, errors.New("Invalid dimensions. Use NdM (e.g. 2d6)")
+	}
+	return RollMultipleDice(number, dimensions)
 }
---
+++ providers/pet/pet.go
@@ -0,0 +1,62 @@
+package pet
+
+import (
+	_ "embed"
+	"encoding/json"
+	"fmt"
+
+	"github.com/maslovpi/odd-character-htmx/functions"
+)
+
+//go:embed data/pets.json
+var petJSON []byte
+
+type (
+	Pet struct {
+		Type     string `json:"type"`
+		Cost     string `json:"cost"`
+		Strength string `json:"str"`
+		Attack   string `json:"attack"`
+	}
+	PetProvider struct {
+		petMap map[string]Pet
+	}
+)
+
+func InitPetProvider() (PetProvider, error) {
+	petMap, err := getPetMap()
+	return PetProvider{petMap: petMap}, err
+}
+
+func getPetMap() (map[string]Pet, error) {
+	var equipmentSlice []Pet
+	err := json.Unmarshal(petJSON, &equipmentSlice)
+	if err != nil {
+		return nil, err
+	}
+
+	petMap := make(map[string]Pet, len(equipmentSlice))
+
+	for _, item := range equipmentSlice {
+		petMap[item.Type] = item
+	}
+	return petMap, nil
+}
+
+func (p *PetProvider) GetPetDescription(petToFind string) (string, error) {
+	item, exists := p.petMap[petToFind]
+	if !exists {
+		return "", nil
+	}
+	rolledStrength, err := functions.RollDice(item.Strength)
+	if err != nil {
+		return "", err
+	}
+	description := fmt.Sprintf(
+		"Cost: %s\nStrength: %d\nAttack: %s",
+		item.Cost,
+		rolledStrength,
+		item.Attack,
+	)
+	return description, nil
+}
